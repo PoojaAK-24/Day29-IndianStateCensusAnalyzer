9d57cf551c519b0b13e41889b6184f07aac34332
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-08-10 14:45:04 +0530

    Pooja Added: Storing data in dictionary

--- IndianStateCensusAnalyzer/CensusAnalyser.cs
+++ IndianStateCensusAnalyzer/CensusAnalyser.cs
@@ -19,7 +19,7 @@ namespace IndianStateCensusAnalyzer
         public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
         {
             //CensusAnalyser obj = new CensusAnalyser();
-            //Console.WriteLine(obj.a);
+            //Console.WriteLine(obj.a); 
             dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
             return dataMap;
         }
