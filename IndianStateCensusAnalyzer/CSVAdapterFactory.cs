3b927872a9f4056a2a0117630111fea3e4397caa
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-08-10 14:47:26 +0530

    Pooja Added:used for multiple country data

--- IndianStateCensusAnalyzer/CSVAdapterFactory.cs
+++ IndianStateCensusAnalyzer/CSVAdapterFactory.cs
@@ -14,7 +14,7 @@ namespace IndianStateCensusAnalyzer
                 case (CensusAnalyser.Country.INDIA):
                     return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
                 //case (CensusAnalyser.Country.US):
-                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);  
                 default:
                     throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
             }
