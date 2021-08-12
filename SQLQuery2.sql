fb2df1039bd5a9d2f95725fbcb656da0e40a1b9d
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-08-12 11:56:40 +0530

    Pooja Added:Testing Custom Exceptions

---
+++ SQLQuery2.sql
@@ -0,0 +1,18 @@
+﻿use payroll_service;
+create table Employee_Payroll1
+(
+EmployeeName varchar(50),
+Gender char(1),
+salary money
+);
+select * from  Employee_Payroll1
+
+create procedure SpAddEmployeeDetails
+@EmployeeName varchar(50),
+@Gender char(1),
+@NetPay money
+as
+insert into Employee_Payroll1 values(@EmployeeName,@Gender,@NetPay);
+go
+
+SpAddEmployeeDetails 'test','M',1234.5;
